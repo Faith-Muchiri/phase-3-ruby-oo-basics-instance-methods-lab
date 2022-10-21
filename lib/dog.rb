7042692e4ce5a852e7a06fbd7f93ceae5a7bf11c
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-10-21 03:41:39 +0300

    complete test

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+    #instance methods
+    def bark
+        puts "Woof!"        
+    end
+    def sit
+        puts "The Dog is sitting"     
+    end
+end
+
+Tommy = Dog.new
+Tommy.bark
+Tommy.sit
